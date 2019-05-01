e1d0b8d28579cbecdafe840702ff952b3e505dd7
Nia Bert <nbert14@gmail.com>
2019-05-01 00:27:25 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_push(array, string)
+  array = []
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  pop_array = []
+  array.pop
+  array.pop 
+
+end
